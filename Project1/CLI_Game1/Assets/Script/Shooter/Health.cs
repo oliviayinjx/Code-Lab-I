461637c3fd3001c11605bc14499a1b25b2207db8
Olivia Yin <66325105+oliviayinjx@users.noreply.github.com>
2022-03-27 20:22:27 -0400

    almost done, need to test

--- Project1/CLI_Game1/Assets/Script/Shooter/Health.cs
+++ Project1/CLI_Game1/Assets/Script/Shooter/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int health = 50;
     [SerializeField] bool isPlayer;
+    public GameObject gameDesigner; 
     //damage dealer and reduce health by damage amount
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -52,7 +53,15 @@ public class Health : MonoBehaviour
         //if player dies, restart the level
         if (!isPlayer && gameObject.name == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            if(SceneManager.GetActiveScene().name == "End")
+            {
+                gameDesigner.GetComponent <SpriteRenderer>().enabled = true; 
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+
         }
         Destroy(gameObject);
     }
